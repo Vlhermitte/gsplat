b9648af78ea3d9be43fde87ffa5d261137e46033
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-05-03 18:43:41 +0200

    disable trajectory rendering during evaluation in simple_trainer.py

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -856,7 +856,7 @@ class Runner:
             # eval the full set
             if step in [i - 1 for i in cfg.eval_steps]:
                 self.eval(step)
-                self.render_traj(step)
+                # self.render_traj(step) Disabled for now
 
             # run compression
             if cfg.compression is not None and step in [i - 1 for i in cfg.eval_steps]:
